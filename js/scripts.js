c0d73ba13c0b42a86233c94f3d443f346eb8dcc5
Damian Juzyk <damian.juzyk@gmail.com>
2017-11-22 18:32:52 +0400

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+// alert('test');
+// the function above has been commented and will  be executed
+
+
+/*
+
+alert('test');
+the function above has been commented and will not be executed
+*/
+
+
+// console.log(comment is blocked);
