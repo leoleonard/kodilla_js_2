321531e9402d1805c0d3162c8ea29d093a38b448
Damian Juzyk <damian.juzyk@gmail.com>
2017-11-23 12:25:38 +0400

    add trangleArea in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,10 @@ the function above has been commented and will not be executed
 
 
 // console.log(comment is blocked);
+
+
+var a = 5;
+var h = 7;
+var triangleArea = a*h/2;
+
+console.log(console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea));
